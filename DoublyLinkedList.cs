a4b3d57a0c798452f393947c1fcde9b8951137bf
Admin <Admin@WIN-KT8KRQ87C7Q>
2022-07-25 14:31:07 +0300

    Добавьте файлы проекта.

---
+++ DoublyLinkedList.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+
+namespace HW_Generics
+{
+    public class DoublyNode<T>
+    {
+        public DoublyNode(T data)
+        {
+            Data = data;
+        }
+        public T Data { get; set; }
+        public DoublyNode<T> Previous { get; set; }
+        public DoublyNode<T> Next { get; set; }
+    }
+    public class DoublyLinkedList<T>
+    {
+        DoublyNode<T> head;
+        DoublyNode<T> tail;
+        int? count;
+
+        public void Add(T data)
+        {
+            DoublyNode<T> node = new DoublyNode<T>(data);
+            if(head == null)
+                head = node;
+            else
+            {
+                tail.Next = node;
+                node.Previous = tail;
+            }
+            tail = node;
+            count++;
+        }
+
+        public void AddFirst(T data)
+        {
+            DoublyNode<T> node = new DoublyNode<T>(data);
+            DoublyNode<T> temp = head;
+            node.Next = temp;
+            head = node;
+            if (count == 0)
+            {
+                tail = head;
+            }
+            else
+                temp.Previous = node;
+            count++;
+        }
+
+        public bool Remove(T data)
+        {
+            DoublyNode<T> current = head;
+
+            while (current != null)
+            {
+                if (current.Data.Equals(data))
+                {
+                    break;
+                }
+                current = current.Next;
+            }
+            if (current != null)
+            {
+                if (current.Next != null)
+                {
+                    current.Next.Previous = current.Previous;
+                }
+                else
+                {
+                    tail = current.Previous;
+                }
+
+                if (current.Previous != null)
+                {
+                    current.Previous.Next = current.Next;
+                }
+                else
+                {
+                    head = current.Next;
+                }
+                count--;
+                return true;
+            }
+            return false;
+        }
+
+        public int Count { get { return (int)count; } }
+        public bool IsEmpty { get { return count == 0; } }
+
+        public void Clear()
+        {
+            count = 0;
+            head = null;
+            tail = null;
+        }
+
+        public bool Contains(T data)
+        {
+            DoublyNode<T> current = head;
+            while(current != null)
+            {
+                if (current.Data.Equals(data))
+                {
+                    return true;
+                    current = current.Next;
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿
+using System;
+using static System.Console;
+using System.Collections.Generic;
+
+namespace HW_Generics
+{
+    internal class Program
+    {
+
+        static void Main()
+        {
+            Problem1();
+            
+
+        }
+
+        public static void Problem1()
+        {
+            int a = 10;
+            int b = 50;
+            WriteLine($"first: {a} second: {b}");
+            Swap<int>(ref a,ref b);
+            WriteLine($"first: {a} second: {b}");
+        }
+
+        public static void Swap<T>(ref T lhs, ref T rhs)
+        {
+            T temp;
+            temp = lhs;
+            lhs = rhs;
+            rhs = temp;
+        }
+
+        public static void Problem2()
+        {
+
+        }
+
+
+
+
+
+
+    }
+}
